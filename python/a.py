c5eda52c6e2a9c84e80ee8164d5608c3e15473b3
glfAdd <44255216+glfAdd@users.noreply.github.com>
2023-03-08 18:24:40 +0800

    fix

--- python/a.py
+++ python/a.py
@@ -1,4 +1,32 @@
-a = (x for x in range(5))
-b = [x for x in range(5)]
-c = {x: x for x in range(5)}
-print(123123)
+# 子生成器
+def test_1():
+    a = 0
+    while True:
+        tmp = yield a
+        if a == 0:
+            break
+        print("%s -----" % tmp)
+        a += 1
+
+
+# 委托生成器
+def test_2():
+    while True:
+        for i in test_1():
+            print(i)
+        # yield from test_1()
+
+# def test_3():
+#     while True:
+
+
+# 调用方
+def main():
+    t = test_2()
+    next(t)
+    print(t.send(10))
+    print(t.send(20))
+
+
+if __name__ == "__main__":
+    main()
